63deacf7a1403691dd6a3801f3bb6ef07f15101b
Ayush Vishwakarma <ayushvishhhhh@gmail.com>
2025-07-29 19:11:13 +0530

    lenis commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
 function updatep() {
     let para = document.getElementById("saletext");
     if(window.innerWidth <= 768){
